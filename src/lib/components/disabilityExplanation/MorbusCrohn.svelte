bc174b3ecc8ccd48bb08d8f76614d5eeb6719b37
Niklas Egger <egger@hdm-stuttgart.de>
2023-12-20 07:36:26 +0100

    add morbus crohn definition

--- src/lib/components/disabilityExplanation/MorbusCrohn.svelte
+++ src/lib/components/disabilityExplanation/MorbusCrohn.svelte
@@ -1,1 +1,21 @@
-<div>Placeholder</div>
+<script>
+	import Sources from '../Sources.svelte';
+</script>
+
+<div>
+	<p>
+		Morbus Crohn ist eine chronische, entzündliche Erkrankung des Verdauungstraktes. Die Krankheit
+		verläuft schubweise und kann von der Mundhöhle bis zum After den gesamten Verdauungstrakt
+		betreffen. Symptome dieser chronischen Erkrankung können beispielsweise starke Bauchschmerzen,
+		Durchfälle, Fieber und Gewichtsverlust sein.
+	</p>
+
+	<Sources
+		sources={[
+			{
+				title: 'Morbus Crohn von der Deutschen Morbus Crohn / Colitis ulcerosa Vereinigung e.V.',
+				url: 'https://www.dccv.de/betroffene-angehoerige/medizinische-grundlagen/was-ist-morbus-crohn/'
+			}
+		]}
+	/>
+</div>
