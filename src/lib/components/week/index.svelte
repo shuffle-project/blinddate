52f8e73a3ca8f03a8b9457f470b9b2e86025bcfb
Niklas Egger <egger@hdm-stuttgart.de>
2024-01-12 14:39:57 +0100

    workaround week component reload stutter

--- src/lib/components/week/index.svelte
+++ src/lib/components/week/index.svelte
@@ -7,21 +7,33 @@
 	export let persona: Persona;
 
 	let desktop = true;
+	let mounted = false;
 
 	onMount(() => {
 		desktop = window.innerWidth > 510;
 		window.addEventListener('resize', () => {
 			desktop = window.innerWidth > 510;
 		});
+		mounted = true;
 	});
 </script>
 
 <!-- CSS media queries not working because the switch in each week component affect ... -->
 <!-- ...  each other through, probably, the bindings -->
-<div class="wrapper">
+<div class="wrapper" class:mounted>
 	{#if desktop}
 		<WeekDesktop {persona} />
 	{:else}
 		<WeekMobile {persona} />
 	{/if}
 </div>
+
+<style lang="scss">
+	.wrapper {
+		opacity: 0%;
+		&.mounted {
+			opacity: 100%;
+			transition: opacity 0.2s ease-in-out;
+		}
+	}
+</style>
