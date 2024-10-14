68411c88b7f070b7f6f76e4c0f12f019881bfc48
Niklas Egger <egger@hdm-stuttgart.de>
2024-10-14 09:40:55 +0200

    change hostname

--- src/lib/constants/hostname.ts
+++ src/lib/constants/hostname.ts
@@ -1,3 +1,3 @@
 // only used for the meta tags
-// export const HOSTNAME = 'https://shuffle-project.github.io/blinddate/';
-export const HOSTNAME = 'https://barrierefreies-blinddate.de/';
+export const HOSTNAME = 'https://shuffle-project.github.io/blinddate/';
+// export const HOSTNAME = 'https://barrierefreies-blinddate.de/';
